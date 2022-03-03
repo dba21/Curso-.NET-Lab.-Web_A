5dab86aae2dcb198a3c28f0d5ebfc0193da885f4
dba21 <barr.azevedo@gmail.com>
2022-03-03 20:18:23 +0000

    Game

--- Game/JS_Street_Fighter.js
+++ Game/JS_Street_Fighter.js
@@ -35,3 +35,41 @@ function hit2(){
         alert("GAME OVER")
     }
 }
+
+
+function heal1(){
+    var value = document.getElementById("myHealth1").clientWidth;
+    var at = document.getElementById("myHealth1").style.width = value +10;
+
+    if(at <= 320){
+        document.getElementById("myHealth1").style.backgroundColor = "green";
+    }
+    else if (at <= 320 && at >= 130){
+        document.getElementById("myHealth1").style.backgroundColor = "yellow";
+    }
+    else if(at >=0){
+        document.getElementById("myHealth1").style.backgroundColor = "red";
+    }
+
+    
+}
+
+function heal2(){
+    var value = document.getElementById("myHealth2").clientWidth;
+    var at = document.getElementById("myHealth2").style.width = value +10;
+
+    if(at <= 320){
+        document.getElementById("myHealth2").style.backgroundColor = "green";
+    }
+    else if (at >= 320 && at <= 130){
+        document.getElementById("myHealth2").style.backgroundColor = "yellow";
+    }
+    else if(at >=0){
+        document.getElementById("myHealth2").style.backgroundColor = "red";
+    }
+    else if(at == 320){
+        break;
+    }
+
+    
+}
