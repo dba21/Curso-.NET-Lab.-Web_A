b178bcfb2a1bdc0d6677aa2f5c57cf7c0c47f93a
dba21 <barr.azevedo@gmail.com>
2022-03-05 17:50:59 +0000

    Atualização
    Incluir botão e funcionalidades para "Super Attack", o nome dos jogadores com uma Popup messages e imagens

--- Game/JS_Street_Fighter.js
+++ Game/JS_Street_Fighter.js
@@ -1,20 +1,52 @@
+function pName1(){
+    let player = prompt("Player, enter your name.");
+    if (player != null){
+        document.getElementById("p1").innerHTML = player;
+    }
+}
+
+function pName2(){
+    let player = prompt("Player, enter your name.");
+    if (player != null){
+        document.getElementById("p2").innerHTML = player;
+    }
+}
+
 function hit1(){
     var value = document.getElementById("myHealth2").clientWidth;
     var at = document.getElementById("myHealth2").style.width = value -10;
     
-
-    if(at >= 320){
+    if(at >= 330){
         document.getElementById("myHealth2").style.backgroundColor = "green";
         //document.getElementById("boneco").src= "Image/dhalsim-street-fighter.png"
     }
-    else if (at <= 320 && at >= 130){
+    else if (at <= 330 && at >= 120){
         document.getElementById("myHealth2").style.backgroundColor = "yellow";
     }
     else if(at >=0){
         document.getElementById("myHealth2").style.backgroundColor = "red";
     }
+    else {
+        alert("Player 1 Win!")
+    }
+}
+
+function hitS1(){
+    var value = document.getElementById("myHealth2").clientWidth;
+    var at = document.getElementById("myHealth2").style.width = value -30;
+    
+    if(at >= 330){
+        document.getElementById("myHealth2").style.backgroundColor = "green";
+        //document.getElementById("boneco").src= "Image/dhalsim-street-fighter.png"
+    }
+    else if (at <= 330 && at >= 120){
+        document.getElementById("myHealth2").style.backgroundColor = "yellow";
+    }
+    else if(at >= 0){
+        document.getElementById("myHealth2").style.backgroundColor = "red";
+    }
     else{
-        alert("GAME OVER")
+        alert("Player 1 Win!")
     }
 }
 
@@ -22,54 +54,71 @@ function hit2(){
     var value = document.getElementById("myHealth1").clientWidth;
     var at = document.getElementById("myHealth1").style.width = value -10;
 
-    if(at >= 320){
+    if(at >= 330){
         document.getElementById("myHealth1").style.backgroundColor = "green";
     }
-    else if (at <= 320 && at >= 130){
+    else if (at <= 330 && at >= 120){
         document.getElementById("myHealth1").style.backgroundColor = "yellow";
     }
     else if(at >=0){
         document.getElementById("myHealth1").style.backgroundColor = "red";
     }
     else{
-        alert("GAME OVER")
+        alert("Player 2 Win!")
     }
 }
 
+function hitS2(){
+    var value = document.getElementById("myHealth1").clientWidth;
+    var at = document.getElementById("myHealth1").style.width = value -30;
+    
+    if(at >= 330){
+        document.getElementById("myHealth1").style.backgroundColor = "green";
+        //document.getElementById("boneco").src= "Image/dhalsim-street-fighter.png"
+    }
+    else if (at <= 330 && at >= 120){
+        document.getElementById("myHealth1").style.backgroundColor = "yellow";
+    }
+    else if(at >= 0){
+        document.getElementById("myHealth1").style.backgroundColor = "red";
+    }
+    else{
+        alert("Player 2 Win!")
+    }
+}
 
 function heal1(){
     var value = document.getElementById("myHealth1").clientWidth;
-    var at = document.getElementById("myHealth1").style.width = value +10;
+    var he = document.getElementById("myHealth1").style.width = value +20;
 
-    if(at <= 320){
+    if(he < 330 && he >= 270){
         document.getElementById("myHealth1").style.backgroundColor = "green";
     }
-    else if (at <= 320 && at >= 130){
+    else if (he >= 120 && he <= 270){
         document.getElementById("myHealth1").style.backgroundColor = "yellow";
     }
-    else if(at >=0){
+    else if(he >=0 && he <= 120){
         document.getElementById("myHealth1").style.backgroundColor = "red";
     }
-
-    
+    else if(he >= 330){
+        document.getElementById("myHealth1").style.width = 330;
+    }
 }
 
 function heal2(){
     var value = document.getElementById("myHealth2").clientWidth;
-    var at = document.getElementById("myHealth2").style.width = value +10;
+    var he = document.getElementById("myHealth2").style.width = value +20;
 
-    if(at <= 320){
+    if(he < 330 && he >= 270){
         document.getElementById("myHealth2").style.backgroundColor = "green";
     }
-    else if (at >= 320 && at <= 130){
+    else if (he >= 120 && he <= 270){
         document.getElementById("myHealth2").style.backgroundColor = "yellow";
     }
-    else if(at >=0){
+    else if(he >=0 && he <= 120){
         document.getElementById("myHealth2").style.backgroundColor = "red";
     }
-    else if(at == 320){
-        break;
+    else if(he >= 330){
+        document.getElementById("myHealth2").style.width = 330;
     }
-
-    
 }
